8fe7cb8185471d28779b8b12685b34cd14b07c71
afouch <afouch1@ahoo.com>
2022-08-08 20:43:27 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api';
 
 function App() {
+  const [host, setHost] = React.useState<string>('');
+  const [name, setName] = React.useState<string>('');
+  const [port, setPort] = React.useState<number | null>(null);
+
+  const handlePortChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+    const num = +e.target.value;
+
+    if (isNaN(num)) {
+      setPort(null);
+    } else {
+      setPort(num);
+    }
+  }
+
+  const sendConfigs = () => {
+    if (!(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.exec(host))) {
+      alert("Invalid host");
+      return;
+    }
+    if (!name) {
+      alert("Invalid name")
+      return;
+    }
+    if (!port || isNaN(port)) {
+      alert("Invalid port")
+      return;
+    }
+    invoke('set_config', {
+      configs: [
+        { 
+          name,
+          server: host,
+          port
+        }
+      ]
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="ip">Host</label>
+      <input onChange={e => setHost(e.target.value)} type="text" name="ip" id="ip" />
+      <br />
+      <label htmlFor="name">Name</label>
+      <input onChange={e => setName(e.target.value)} type="text" name="name" id="name" />
+      <br />
+      <label htmlFor="port">Port</label>
+      <input onChange={handlePortChange} type="number" name="port" id="port" />
+      <br />
+      <button onClick={sendConfigs}>Click</button>
     </div>
   );
 }
